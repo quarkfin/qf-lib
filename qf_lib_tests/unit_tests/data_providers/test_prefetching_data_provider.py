73ef026fdbf28ca0cf72c2d5f9af2f18284273da
Jacek Witkowski <jacek.witkowski@cern.ch>
2018-07-31 17:13:14 +0200

    Fixed tests for PrefetchingDataProvider.

--- qf_lib_tests/unit_tests/data_providers/test_prefetching_data_provider.py
+++ qf_lib_tests/unit_tests/data_providers/test_prefetching_data_provider.py
@@ -8,7 +8,7 @@ import qf_lib.testing_tools.containers_comparison as tt
 from qf_lib.common.enums.price_field import PriceField
 from qf_lib.common.tickers.tickers import BloombergTicker
 from qf_lib.common.utils.dateutils.relative_delta import RelativeDelta
-from qf_lib.containers.dataframe.qf_dataframe import QFDataFrame
+from qf_lib.containers.dataframe.prices_dataframe import PricesDataFrame
 from qf_lib.data_providers.prefetching_data_provider import PrefetchingDataProvider
 from qf_lib.data_providers.price_data_provider import DataProvider
 
@@ -50,14 +50,14 @@ class TestPrefetchingDataProvider(unittest.TestCase):
         actual_frame = self.prefetching_data_provider.get_price(
             self.msft_ticker, self.cached_fields, self.start_date, self.end_date)
 
-        expected_frame = QFDataFrame(index=self.cached_dates, columns=self.cached_fields)
+        expected_frame = PricesDataFrame(index=self.cached_dates, columns=self.cached_fields)
 
         tt.assert_dataframes_equal(expected_frame, actual_frame, check_index_type=True, check_column_type=True)
 
     def test_get_price_with_single_field(self):
         actual_frame = self.prefetching_data_provider.get_price(
             self.cached_tickers, PriceField.Volume, self.start_date, self.end_date)
-        expected_frame = QFDataFrame(index=self.cached_dates, columns=self.cached_tickers)
+        expected_frame = PricesDataFrame(index=self.cached_dates, columns=self.cached_tickers)
 
         tt.assert_dataframes_equal(expected_frame, actual_frame, check_index_type=True, check_column_type=True)
 
@@ -65,7 +65,7 @@ class TestPrefetchingDataProvider(unittest.TestCase):
         actual_frame = self.prefetching_data_provider.get_price(
             self.cached_tickers, self.cached_fields, self.start_date, self.start_date)
 
-        expected_frame = QFDataFrame(index=self.cached_tickers, columns=self.cached_fields)
+        expected_frame = PricesDataFrame(index=self.cached_tickers, columns=self.cached_fields)
 
         tt.assert_dataframes_equal(expected_frame, actual_frame, check_index_type=True, check_column_type=True)
 
