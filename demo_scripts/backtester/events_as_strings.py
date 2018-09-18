daa934765a2c48a3914c7f97e55ca5f67f6040d6
Jacek Witkowski <jacek.witkowski@cern.ch>
2018-09-18 14:48:40 +0200

    Fixed a demo script.

--- demo_scripts/backtester/events_as_strings.py
+++ demo_scripts/backtester/events_as_strings.py
@@ -1,9 +1,8 @@
 from qf_lib.backtesting.transaction import Transaction
-from qf_lib.common.tickers.tickers import BloombergTicker
 from qf_lib.common.utils.dateutils.string_to_date import str_to_date
 
 time = str_to_date("2010-01-01")
-ticker = BloombergTicker("MSFT US Equity")
+ticker = Contract("MSFT US Equity")
 quantity = 13
 price = 100.5
 commission = 1.2
