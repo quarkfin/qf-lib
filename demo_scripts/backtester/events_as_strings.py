c82202f1f1b61938874169f25b6f9efe1bfe1bf3
Jacek Witkowski <jacek.witkowski@cern.ch>
2018-09-19 12:04:41 +0200

    Small cosmetic change.

--- demo_scripts/backtester/events_as_strings.py
+++ demo_scripts/backtester/events_as_strings.py
@@ -1,8 +1,9 @@
+from qf_lib.backtesting.contract.contract import Contract
 from qf_lib.backtesting.transaction import Transaction
 from qf_lib.common.utils.dateutils.string_to_date import str_to_date
 
 time = str_to_date("2010-01-01")
-ticker = Contract("MSFT US Equity")
+ticker = Contract("MSFT US Equity", security_type='SEK', exchange='TEST_XCHANGE')
 quantity = 13
 price = 100.5
 commission = 1.2
