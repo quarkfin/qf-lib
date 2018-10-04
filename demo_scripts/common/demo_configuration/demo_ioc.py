4077532f8a021fcf7e723b42c8303ca48f5d2ee2
Jacek Witkowski <jacek.witkowski@cern.ch>
2018-10-04 15:29:41 +0200

    Added EmailPublisher to demo_ioc.py.

--- demo_scripts/common/demo_configuration/demo_ioc.py
+++ demo_scripts/common/demo_configuration/demo_ioc.py
@@ -11,24 +11,6 @@ def _get_ioc_container():
     from dic.container import ContainerBuilder
     from dic.scope import SingleInstance
 
-    # from cern_analytics.data_providers.current_sensitivities_dao import CurrentSensitivitiesDAO
-    # from cern_analytics.data_providers.daily_report_dao import DailyReportDAO
-    # from cern_analytics.data_providers.db_connection_provider import ReadOnlyDBConnectionProvider
-    # from cern_analytics.data_providers.db_connection_provider import ReadWriteDBConnectionProvider
-    # from cern_analytics.data_providers.ststmunich_position_dao import StStMunichPositionDAO
-    # from cern_analytics.data_providers.true_view.detailed_risk_report_dao import DetailedRiskReportDAO
-    # from cern_analytics.data_providers.true_view.trueview_monthly_scenarios_dao import TrueViewMonthlyScenariosDAO
-    # from cern_analytics.history_simulation.fund_history_simulator import FundHistorySimulator
-    # from cern_analytics.report_generators.sensitivities_report.report_data_downloader import \
-    #     SensitivityReportDataDownloader
-    # from cern_analytics.report_generators.sensitivities_report.report_database_exporter import \
-    #     SensitivitiesDatabaseExporter
-    # from cern_analytics.report_generators.sensitivities_report.report_factory import SensitivitiesReportFactory
-    # from cern_analytics.report_generators.sensitivities_report.report_pdf_exporter import SensitivitiesPDFExporter
-    # from cern_analytics.report_generators.sensitivities_report.subreport_factory.subreport_factory import \
-    #     SensitivitiesSubreportFactory
-    # from cern_analytics.utils.convexity_periods_calculator import ConvexityPeriodsCalculator
-    # from geneva_analytics.data_providers.timeseries_data_provider import TimeseriesDataProvider
     # from qf_lib.common.risk_parity_boxes.risk_parity_boxes import RiskParityBoxesFactory
     # from qf_lib.common.rolling_contracts.rolling_contracts_series_producer import RollingContractsSeriesProducer
     # from qf_lib.common.utils.dateutils.timer import RealTimer
@@ -42,13 +24,13 @@ def _get_ioc_container():
     from qf_lib.data_providers.general_price_provider import GeneralPriceProvider
     # from qf_lib.data_providers.haver import HaverDataProvider
     from qf_lib.data_providers.quandl.quandl_data_provider import QuandlDataProvider
-    # from qf_lib.publishers.email_publishing.email_publisher import EmailPublisher
+    from qf_lib.publishers.email_publishing.email_publisher import EmailPublisher
     from qf_lib.settings import Settings
 
     builder = ContainerBuilder()
 
     # # PUBLISHERS
-    # builder.register_class(EmailPublisher, component_scope=SingleInstance)
+    builder.register_class(EmailPublisher, component_scope=SingleInstance)
     #
     # # DATA PROVIDERS
     # builder.register_class(ReadOnlyDBConnectionProvider, component_scope=SingleInstance)
