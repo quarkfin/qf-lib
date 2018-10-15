2d4631370ddf2bccf8223a94ad11089d67b155e1
okalinow <olga.kalinowska@cern.ch>
2018-10-15 16:17:23 +0200

    Added transaction costs to close_open_gap_demo

--- qf_lib_tests/unit_tests/common/utils/test_close_open_gap.py
+++ qf_lib_tests/unit_tests/common/utils/test_close_open_gap.py
@@ -6,10 +6,10 @@ from pandas import date_range
 from qf_lib.common.enums.price_field import PriceField
 from qf_lib.containers.dataframe.prices_dataframe import PricesDataFrame
 from qf_lib.testing_tools.containers_comparison import assert_lists_equal
-from scripts.analysis.ensamble_avg_vs_time_avg.open_close_gap import open_close_gap
+from scripts.analysis.ensamble_avg_vs_time_avg.close_open_gap import close_open_gap
 
 
-class TestOpenCloseGapUtils(TestCase):
+class TestCloseOpenGapUtils(TestCase):
     def setUp(self):
         self.tms = date_range('1991-05-14', periods=6, freq='D')
         open = [100, 100, 100, 101, 101, 102]
@@ -20,5 +20,5 @@ class TestOpenCloseGapUtils(TestCase):
 
     def test_open_close_gap(self):
         expected_values = [1.00, 1.00, 1.00, 1.01, 1.01, 1.02]
-        actual_values = open_close_gap(self.prices_df)
+        actual_values = close_open_gap(self.prices_df)
         assert_lists_equal(expected_values, actual_values)
