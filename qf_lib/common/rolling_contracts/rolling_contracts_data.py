2c2b9990d6de53537fd99c8bbf5db6b00bbf45fe
Jacek Witkowski <jacek.witkowski@cern.ch>
2018-11-19 10:44:10 +0100

    Updated RollingContractData so that it has OHLCV instead of just Close prices.

--- qf_lib/common/rolling_contracts/rolling_contracts_data.py
+++ qf_lib/common/rolling_contracts/rolling_contracts_data.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
-from qf_lib.containers.series.prices_series import PricesSeries
+from qf_lib.containers.dataframe.prices_dataframe import PricesDataFrame
 from qf_lib.containers.series.simple_returns_series import SimpleReturnsSeries
 
 
 class RollingContractData(object):
-    def __init__(self, prices_tms: PricesSeries, time_to_expiration_tms: pd.Series, returns_tms: SimpleReturnsSeries):
-        self.prices_tms = prices_tms
+    def __init__(self, prices_df: PricesDataFrame, time_to_expiration_tms: pd.Series, returns_tms: SimpleReturnsSeries):
+        """
+        Parameters
+        ----------
+        prices_df
+            DataFrame for a certain contract (e.g. 1-month contract) with Open, High, Low, Close, Volume as columns.
+            It is indexed with time.
+        time_to_expiration_tms
+            Timeseries showing how in much time the currently held real contract will expire
+        returns_tms
+            returns of the
+        """
+        self.prices_df = prices_df
         self.time_to_expiration_tms = time_to_expiration_tms
         self.returns_tms = returns_tms
