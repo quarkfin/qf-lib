096d97984b401c6c63354c9e585883a6c2eab6dd
okalinow <olga.kalinowska@cern.ch>
2018-11-29 14:52:08 +0100

    Params in param_estimation pdfs are named

--- qf_lib/analysis/model_params_estimation/evaluation_utils.py
+++ qf_lib/analysis/model_params_estimation/evaluation_utils.py
@@ -1,16 +1,15 @@
 from typing import List, Sequence
 
-from geneva_analytics.backtesting.alpha_models_testers.backtest_summary import BacktestSummary, BacktestSummaryElement
+from geneva_analytics.backtesting.alpha_models_testers.backtest_summary import BacktestSummary
 from qf_lib.common.enums.trade_field import TradeField
 from qf_lib.common.tickers.tickers import Ticker
 from qf_lib.common.utils.dateutils.date_to_string import date_to_str
 from qf_lib.common.utils.document_exporting import Document, ParagraphElement, HeadingElement
 from qf_lib.common.utils.document_exporting.element.new_page import NewPageElement
 from qf_lib.common.utils.returns.sqn import sqn, avg_nr_of_trades_per1y, trade_based_cagr, trade_based_max_drawdown
-from qf_lib.containers.dataframe.qf_dataframe import QFDataFrame
 
 
-def add_backtest_description(document: Document, backtest_result: BacktestSummary):
+def add_backtest_description(document: Document, backtest_result: BacktestSummary, param_names: List[str]):
     """
     Adds verbal description of the backtest to the document. The description will be placed on a single page.
     """
@@ -35,7 +34,7 @@ def add_backtest_description(document: Document, backtest_result: BacktestSummar
     document.add_element(HeadingElement(2, "Parameters Tested"))
     for param_index, param_list in enumerate(backtest_result.parameters_tested):
         param_list_str = ", ".join(map(str, param_list))
-        document.add_element(ParagraphElement("Parameter #{} = [{}]".format(param_index + 1, param_list_str)))
+        document.add_element(ParagraphElement("{} = [{}]".format(param_names[param_index], param_list_str)))
 
     document.add_element(NewPageElement())
 
