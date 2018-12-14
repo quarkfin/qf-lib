c3b9d6660d4786fb8f2cdf997cd37ff103fc7f4e
Marcin Borratynski <marcin.borratynski@cern.ch>
2018-12-14 16:04:22 +0100

    implemented live trading session

--- qf_lib/backtesting/trading_session/live_trading_session.py
+++ qf_lib/backtesting/trading_session/live_trading_session.py
@@ -1,34 +1,19 @@
 from dic.container import Container
 
-
-from qf_lib.backtesting.backtest_result.backtest_result import BacktestResult
-from qf_lib.backtesting.broker.backtest_broker import BacktestBroker
-from qf_lib.backtesting.contract_to_ticker_conversion.base import ContractTickerMapper
+from qf_lib.backtesting.contract_to_ticker_conversion.vol_strategy_mapper import VolStrategyContractTickerMapper
 from qf_lib.backtesting.data_handler.data_handler import DataHandler
-from qf_lib.backtesting.events.event_manager import EventManager
-from qf_lib.backtesting.events.notifiers import Notifiers
-from qf_lib.backtesting.events.time_flow_controller import BacktestTimeFlowController
-from qf_lib.backtesting.execution_handler.simulated.commission_models.fixed_commission_model import FixedCommissionModel
-from qf_lib.backtesting.execution_handler.simulated.simulated_execution_handler import SimulatedExecutionHandler
-from qf_lib.backtesting.execution_handler.simulated.slippage.price_based_slippage import PriceBasedSlippage
-from qf_lib.backtesting.monitoring.backtest_monitor import BacktestMonitor
-from qf_lib.backtesting.monitoring.light_backtest_monitor import LightBacktestMonitor
 from qf_lib.backtesting.monitoring.live_trading_monitor import LiveTradingMonitor
 from qf_lib.backtesting.order.orderfactory import OrderFactory
-from qf_lib.backtesting.portfolio.portfolio import Portfolio
-from qf_lib.backtesting.portfolio.portfolio_handler import PortfolioHandler
 from qf_lib.backtesting.position_sizer.simple_position_sizer import SimplePositionSizer
-from qf_lib.backtesting.risk_manager.naive_risk_manager import NaiveRiskManager
 from qf_lib.backtesting.trading_session.trading_session import TradingSession
-from qf_lib.common.utils.dateutils.date_to_string import date_to_str
-from qf_lib.common.utils.dateutils.timer import SettableTimer, RealTimer
+from qf_lib.common.utils.dateutils.timer import RealTimer
 from qf_lib.common.utils.document_exporting.pdf_exporter import PDFExporter
 from qf_lib.common.utils.excel.excel_exporter import ExcelExporter
 from qf_lib.common.utils.logging.qf_parent_logger import qf_logger
 from qf_lib.data_providers.bloomberg import BloombergDataProvider
-from qf_lib.data_providers.price_data_provider import DataProvider
 from qf_lib.interactive_brokers.ib_broker import IBBroker
 from qf_lib.settings import Settings
+
 
 class LiveTradingSession(TradingSession):
     """
@@ -37,95 +22,42 @@ class LiveTradingSession(TradingSession):
 
     def __init__(self, trading_session_name: str, container: Container):
         """
-        Set up the backtest variables according to what has been passed in.
+        Set up the configuration of all elements.
         """
         self.logger = qf_logger.getChild(self.__class__.__name__)
         self.trading_session_name = trading_session_name
 
-        settings = container.resolve(Settings)  # type: Settings
-        data_provider = container.resolve(BloombergDataProvider)  # type: BloombergDataProvider
-        pdf_exporter = container.resolve(PDFExporter)  # type: PDFExporter
-        excel_exporter = container.resolve(ExcelExporter)  # type: ExcelExporter
+        self.settings = container.resolve(Settings)                     # type: Settings
+        self.data_provider = container.resolve(BloombergDataProvider)   # type: BloombergDataProvider
+        self.pdf_exporter = container.resolve(PDFExporter)              # type: PDFExporter
+        self.excel_exporter = container.resolve(ExcelExporter)          # type: ExcelExporter
 
-        timer = RealTimer()
-        data_handler = DataHandler(data_provider, timer)
-        monitor = LiveTradingMonitor(settings, pdf_exporter, excel_exporter)
-        broker = IBBroker()
+        self.timer = RealTimer()
+        self.data_handler = DataHandler(self.data_provider, self.timer)
+        self.monitor = LiveTradingMonitor(self.settings, self.pdf_exporter, self.excel_exporter)
+        self.broker = IBBroker()
 
-        order_factory = OrderFactory(broker, data_handler, contract_ticker_mapper)
+        self.contract_ticker_mapper = VolStrategyContractTickerMapper()
+        self.order_factory = OrderFactory(self.broker, self.data_handler, self.contract_ticker_mapper)
+        self.position_sizer = SimplePositionSizer(self.broker, self.data_handler, self.order_factory,
+                                                  self.contract_ticker_mapper)
 
         self.logger.info(
             "\n".join([
-                "Creating Backtest Trading Session.",
+                "Creating Backtest Trading Session: ",
                 "\tTrading Session Name: {}".format(trading_session_name),
-                "\tSettings: {}".format(settings),
-                "\tSettings: {}".format(settings),
-                "\tPFD Exporter: {}".format(pdf_exporter.__class__.__name__),
-                "\tExcel Exporter: {}".format(excel_exporter.__class__.__name__),
+                "\tSettings: {}".format(self.settings),
+                "\tData Provider: {}".format(self.data_provider),
+                "\tPDF Exporter: {}".format(self.pdf_exporter),
+                "\tExcel Exporter: {}".format(self.excel_exporter),
+                "\tData Handler: {}".format(self.data_handler),
+                "\tMonitor: {}".format(self.monitor),
+                "\tBroker: {}".format(self.broker),
+                "\tContract-Ticker Mapper: {}".format(self.contract_ticker_mapper),
+                "\tOrder Factory: {}".format(self.order_factory),
+                "\tPosition Sizer: {}".format(self.position_sizer)
             ])
         )
-
-
-
-
-
-        time_flow_controller = BacktestTimeFlowController(
-            notifiers.scheduler, events_manager, timer, notifiers.empty_queue_event_notifier, end_date
-        )
-        portfolio_handler = PortfolioHandler(portfolio, monitor, notifiers.scheduler)
-        position_sizer = SimplePositionSizer(broker, data_handler, order_factory, contract_ticker_mapper)
-
-        self.logger.info(
-            "\n".join([
-                "Configuration of components:",
-                "\tPosition sizer: {:s}".format(position_sizer.__class__.__name__),
-                "\tTimer: {:s}".format(timer.__class__.__name__),
-                "\tRisk Manager: {:s}".format(risk_manager.__class__.__name__),
-                "\tData Handler: {:s}".format(data_handler.__class__.__name__),
-                "\tBacktest Result: {:s}".format(backtest_result.__class__.__name__),
-                "\tMonitor: {:s}".format(monitor.__class__.__name__),
-                "\tExecution Handler: {:s}".format(execution_handler.__class__.__name__),
-                "\tSlippage Model: {:s}".format(slippage_model.__class__.__name__),
-                "\tCommission Model: {:s}".format(commission_model.__class__.__name__),
-                "\tBroker: {:s}".format(broker.__class__.__name__),
-            ])
-        )
-
-        self.notifiers = notifiers
-
-        self.event_manager = events_manager
-        self.data_handler = data_handler
-        self.portfolio = portfolio
-        self.portfolio_handler = portfolio_handler
-        self.execution_handler = execution_handler
-        self.position_sizer = position_sizer
-        self.risk_manager = risk_manager
-        self.monitor = monitor
-        self.timer = timer
-        self.order_factory = order_factory
-        self.time_flow_controller = time_flow_controller
-        self.broker = broker
-
-    @staticmethod
-    def _create_event_manager(timer, notifiers: Notifiers):
-        events_manager = EventManager(timer)
-
-        events_manager.register_notifiers([
-            notifiers.all_event_notifier,
-            notifiers.empty_queue_event_notifier,
-            notifiers.end_trading_event_notifier,
-            notifiers.scheduler
-        ])
-        return events_manager
 
     def start_trading(self) -> None:
-        """
-        Carries out an while loop that processes incoming events. The loop continues until the EndTradingEvent occurs
-        (e.g. no more data for the backtest).
-        """
-        self.logger.info("Running backtest...")
-        while self.event_manager.continue_trading:
-            self.event_manager.dispatch_next_event()
-
-        self.logger.info("Backtest finished, generating report...")
-        self.monitor.end_of_trading_update()
+        raise NotImplementedError("This method should not be used in live trading")
