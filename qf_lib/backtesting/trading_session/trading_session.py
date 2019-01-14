c84ca443f0ad39c203cfcdf583893362fc505f24
Marcin Borratynski <marcin.borratynski@cern.ch>
2019-01-14 14:51:53 +0100

    Updated trading session - always use start trading

--- qf_lib/backtesting/trading_session/trading_session.py
+++ qf_lib/backtesting/trading_session/trading_session.py
@@ -3,11 +3,13 @@ from abc import ABCMeta, abstractmethod
 from qf_lib.backtesting.broker.broker import Broker
 from qf_lib.backtesting.contract_to_ticker_conversion.base import ContractTickerMapper
 from qf_lib.backtesting.data_handler.data_handler import DataHandler
+from qf_lib.backtesting.events.event_manager import EventManager
 from qf_lib.backtesting.events.notifiers import Notifiers
 from qf_lib.backtesting.monitoring.abstract_monitor import AbstractMonitor
 from qf_lib.backtesting.order.orderfactory import OrderFactory
 from qf_lib.backtesting.position_sizer.position_sizer import PositionSizer
 from qf_lib.common.utils.dateutils.timer import Timer
+from qf_lib.common.utils.logging.qf_parent_logger import qf_logger
 from qf_lib.data_providers.price_data_provider import DataProvider
 from qf_lib.settings import Settings
 
@@ -30,12 +32,20 @@ class TradingSession(object, metaclass=ABCMeta):
         self.contract_ticker_mapper = None  # type: ContractTickerMapper
         self.order_factory = None           # type: OrderFactory
         self.position_sizer = None          # type: PositionSizer
+        self.event_manager = None           # type: EventManager
 
         self.notifiers = None               # type: Notifiers
 
+        self.logger = qf_logger.getChild(self.__class__.__name__)
 
-    @abstractmethod
     def start_trading(self) -> None:
         """
-        Run this method in order to perform historical backtest
+        Carries out an while loop that processes incoming events. The loop continues until the EndTradingEvent occurs
+        (e.g. no more data for the backtest).
         """
+        self.logger.info("Running backtest...")
+        while self.event_manager.continue_trading:
+            self.event_manager.dispatch_next_event()
+
+        self.logger.info("Backtest finished, generating report...")
+        self.monitor.end_of_trading_update()
