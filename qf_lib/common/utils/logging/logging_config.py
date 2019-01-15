bcf287a7cfffd3ec068b51f515a37044ef17c89a
Marcin Borratynski <marcin.borratynski@cern.ch>
2019-01-15 16:19:09 +0100

    ib logger - logger config updated

--- qf_lib/common/utils/logging/logging_config.py
+++ qf_lib/common/utils/logging/logging_config.py
@@ -6,16 +6,21 @@ from os import makedirs
 from os.path import exists, join
 
 from qf_lib.common.utils.helpers import get_formatted_filename
-from qf_lib.common.utils.logging.qf_parent_logger import qf_logger
+from qf_lib.common.utils.logging.qf_parent_logger import qf_logger, ib_logger
 from qf_lib.get_sources_root import get_src_root
 
 
 def setup_logging(level, console_logging=True, log_dir=None, log_file_base_name=None):
+    _inner_setup_logging(qf_logger, level, console_logging, log_dir, "QF_" + log_file_base_name)
+    _inner_setup_logging(ib_logger, level, console_logging, log_dir, "IB_" + log_file_base_name)
+
+
+def _inner_setup_logging(logger, level, console_logging, log_dir, log_file_base_name):
     formatter = logging.Formatter(
         fmt='%(asctime)s %(levelname)s [%(name)s]: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
-    qf_logger.setLevel(level)
+    logger.setLevel(level)
 
     # config logging to file
     if log_dir is not None:
@@ -27,10 +32,10 @@ def setup_logging(level, console_logging=True, log_dir=None, log_file_base_name=
 
         file_handler = logging.FileHandler(join(abs_log_dir, log_file))
         file_handler.setFormatter(formatter)
-        qf_logger.addHandler(file_handler)
+        logger.addHandler(file_handler)
 
     # config logging to console (stdout)
     if console_logging:
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
-        qf_logger.addHandler(stream_handler)
+        logger.addHandler(stream_handler)
