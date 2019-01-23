2653a8addfeeee73788afa75f5b517807eeda1e4
okalinow <olga.kalinowska@cern.ch>
2019-01-23 15:29:33 +0100

    deleted another unimportant file

--- qf_lib/backtesting/alpha_models_testers/alpha_model_factory.py
+++ qf_lib/backtesting/alpha_models_testers/alpha_model_factory.py
@@ -1,6 +1,5 @@
 from typing import Type
 
-from geneva_analytics.backtesting.alpha_models.vol_long_short.vol_long_short import VolLongShort
 from qf_lib.backtesting.alpha_model.alpha_model import AlphaModel
 from qf_lib.backtesting.data_handler.data_handler import DataHandler
 
@@ -12,9 +11,3 @@ class AlphaModelFactory(object):
     def make_model(self, model_type: Type[AlphaModel], *params, risk_estimation_factor=None):
         model = model_type(*params, risk_estimation_factor=risk_estimation_factor, data_handler=self.data_handler)
         return model
-
-    def make_parametrized_vol_long_short_model(self):
-        params = (4.5, 2, 5)
-        risk_param = 1.25
-        return VolLongShort(*params, risk_estimation_factor=risk_param, data_handler=self.data_handler)
-
