768ef7800e764f4af02de86c3a293f75fea5577f
Marcin Borratynski <marcin.borratynski@cern.ch>
2019-03-21 15:56:50 +0100

    Finished Live trading Monitor, not tested due to bbg request limit

--- demo_scripts/charts/cone_chart.py
+++ demo_scripts/charts/cone_chart.py
@@ -8,13 +8,13 @@ from qf_lib.data_providers.general_price_provider import GeneralPriceProvider
 from qf_lib.plotting.charts.cone_chart import ConeChart
 
 start_date = str_to_date('1996-01-01')
-end_date = str_to_date('2012-02-01')
+end_date = str_to_date('2014-02-01')
 live_start_date = str_to_date('2012-01-01')
 
 data_provider = ioc.container.resolve(GeneralPriceProvider)
 spy_tms = data_provider.get_price(QuandlTicker('AAPL', 'WIKI'), PriceField.Close, start_date, end_date)
 
-cone_chart = ConeChart(data=spy_tms, nr_of_data_points=20, is_end_date=live_start_date)
+cone_chart = ConeChart(data=spy_tms, nr_of_data_points=800, is_end_date=live_start_date)
 cone_chart.plot()
 
 plt.show(block=True)
