2a469a58bc77a00f69cdcb4dbbb515be0ad23013
okalinow <olga.kalinowska@cern.ch>
2019-03-27 16:20:17 +0100

    garch script and others ready to use

--- qf_lib/common/utils/miscellaneous/annualise_with_sqrt.py
+++ qf_lib/common/utils/miscellaneous/annualise_with_sqrt.py
@@ -1,31 +1,36 @@
 from math import sqrt
+from typing import Union, List
+
+from pandas import Series
 
 from qf_lib.common.enums.frequency import Frequency
 
 
-def annualise_with_sqrt(values, frequency: Frequency) -> float:
+def annualise_with_sqrt(values: Union[float, Series, List[float]], frequency: Frequency) \
+        -> Union[float, Series, List[float]]:
     """
     Annualises samples of given frequency by multiplying each value by the square-rooted number of samples in a year
     for a given frequency.
 
     Parameters
     ----------
-    values : float/List[float]
+    values
         value(s) to annualise
-    frequency : Frequency
+    frequency
         original sampling frequency for the given value(s)
 
     Returns
     -------
-    annualised_values : float/List[float]
+    annualised_values
         annualised values
     """
 
-    is_scalar = isinstance(values, float) or isinstance(values, int)
     occurrences_in_year = frequency.occurrences_in_year
 
-    if is_scalar:
+    if isinstance(values, float) or isinstance(values, int):
         annualised_values = values * sqrt(occurrences_in_year)
+    elif isinstance(values, Series):
+        annualised_values = values.multiply(sqrt(occurrences_in_year))
     else:
         annualised_values = [value * sqrt(occurrences_in_year) for value in values]
 
--- qf_lib/common/utils/volatility/volatility_forecast.py
+++ qf_lib/common/utils/volatility/volatility_forecast.py
@@ -2,50 +2,112 @@ import numpy as np
 from arch.univariate import ConstantMean, Normal
 from arch.univariate.volatility import VolatilityProcess
 
+from qf_lib.common.enums.frequency import Frequency
+from qf_lib.common.utils.miscellaneous.annualise_with_sqrt import annualise_with_sqrt
+from qf_lib.containers.series.qf_series import QFSeries
 from qf_lib.containers.series.simple_returns_series import SimpleReturnsSeries
 
 
-class VolatilityForecast(object):
-    def __init__(self, returns: SimpleReturnsSeries):
+class VolatilityForecast(object):  # todo: make all other scripts compatible
+    def __init__(self, returns_tms: SimpleReturnsSeries, vol_process: VolatilityProcess, window_len: int = 2016,
+                 horizon: int = 1, method: str = 'analytic', annualise: bool = True, frequency: Frequency = None):
         """
-        Creates a class used for vol forecasting
-        returns
+        Creates class used for vol forecasting: describes the volatility forecast configuration as well as the input
+        and output data (output is created and assigned by calling one of the class methods).
+
+        An instance of this class should describe one specific forecast and store its parameters.
+        Parameters should NOT be modified after calling calculate_timeseries() method.  # todo: find a way to prevent it
+
+        Parameters
+        ----------
+        returns_tms
             series of simple returns of the asset
-        frequency
-            frequency fo the returns in the series
-        """
-        self.returns = returns
-
-    def calculate_vol_forecast(self, vol_process: VolatilityProcess, horizon: int, method: str) -> float:
-        """
-        Calculates volatility forecast for single asset. Value is NOT annualised.
-        It is expressed in the frequency of returns
-
         vol_process
-            volatility proces used for forecasting. For example EGARCH(p=p, o=o, q=q)
+            volatility process used for forecasting. For example EGARCH(p=p, o=o, q=q)
+        window_len
+            number of rows used as input data to calculate forecasted volatility
         horizon
             horizon for the volatility forecast. It is expressed in the frequency of the returns provided
         method
             method of forecast calculation. Possible: 'analytic', 'simulation' or 'bootstrap'
             'analytic' is the fastest but is not available for EGARCH and possibly some other models.
             For details check arch documentation
+        annualise
+            flag indicating whether the result is annualised; True by default
+        frequency
+            if annualise is True, this should be the frequency of the returns timeseries that is provided as input;
+            not used if annualise is False.
 
-        :returns
-        forecasted value of the volatility (not annualised)
         """
-        am = self._get_ARCH_model(vol_process)
-        res = am.fit(disp='off')
-        forecasts = res.forecast(horizon=horizon, method=method)
-        column_str = 'h.{}'.format(horizon)  # take value for the selected horizon
+        self.vol_process = vol_process
+        self.window_len = window_len
+        self.horizon = horizon
+        self.method = method
+
+        assert not annualise or frequency is not None
+        self.annualise = annualise
+        self.frequency = frequency
+
+        self.returns_tms = returns_tms
+        self.forecasted_volatility = None  # will be assigned in the calculate_timeseries() method
+
+    def calculate_timeseries(self) -> QFSeries:
+        """
+        Calculates volatility forecast for single asset. It is expressed in the frequency of returns.
+        Value is calculated based on the configuration as in the object attributes. The result of the calculation
+        is returned as well as assigned as self.forecasted_volatility object attribute.
+
+        Returns
+        -------
+        Forecasted value of the volatility.
+        """
+
+        assert self.forecasted_volatility is None, "Forecast can be calculated "
+
+        # if multiply_by_const:  # todo
+        #     returns = returns * multiplier
+
+        volatility_tms = self.returns_tms.rolling_window(self.window_len, self._calculate_single_value)
+        volatility_tms = volatility_tms.shift(self.horizon)
+        volatility_tms = volatility_tms.dropna()
+
+        if self.annualise:
+            volatility_tms = annualise_with_sqrt(volatility_tms, self.frequency)
+
+        # if multiply_by_const:  # todo
+        #     vol = vol / multiplier
+
+        self.forecasted_volatility = volatility_tms
+        return volatility_tms
+
+    def _calculate_single_value(self, returns: SimpleReturnsSeries) -> float:
+        """
+        Calculates volatility forecast for single asset. Value is NOT annualised.
+        It is expressed in the frequency of returns
+
+        Parameters
+        ----------
+        returns
+            series of simple returns of the asset
+
+        Returns
+        -------
+        Forecasted value of the volatility (not annualised).
+        """
+
+        am = self._get_ARCH_model(returns, self.vol_process)
+        res = am.fit(disp='off', show_warning=False)  # options={'maxiter': 10000, 'ftol': 1e-2})
+        forecasts = res.forecast(horizon=self.horizon, method=self.method)
+        column_str = 'h.{}'.format(self.horizon)  # take value for the selected horizon
         forecasts_series = forecasts.variance[column_str]
         # take the last value (most recent forecast)
         forecasted_value = forecasts_series[-1]
         # forecasted_value corresponds to variance. Convert to volatility
-        forecasted_vol = np.sqrt(forecasted_value)
+        forecasted_vol = np.sqrt(forecasted_value)  # ** (1.0/float(self.vol_process.))  # todo: works only for power=2
         return forecasted_vol
 
-    def _get_ARCH_model(self, vol_process: VolatilityProcess):
-        am = ConstantMean(self.returns)
+    def _get_ARCH_model(self, returns: SimpleReturnsSeries, vol_process: VolatilityProcess):
+        am = ConstantMean(returns)
         am.volatility = vol_process
         am.distribution = Normal()
         return am
