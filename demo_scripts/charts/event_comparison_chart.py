126dcab6fc838d4929379b47f4a3819f2e358176
Marcin Borratynski <marcin.borratynski@cern.ch>
2019-03-29 18:19:30 +0100

    fixed imports in many charts

--- demo_scripts/charts/event_comparison_chart.py
+++ demo_scripts/charts/event_comparison_chart.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-import qf_common.config.ioc as ioc
+from demo_scripts.demo_configuration.demo_ioc import container
 from qf_lib.common.enums.price_field import PriceField
 from qf_lib.common.enums.rebase_method import RebaseMethod
 from qf_lib.common.tickers.tickers import QuandlTicker
@@ -13,7 +13,7 @@ from qf_lib.plotting.helpers.create_event_comparison_chart import create_event_c
 start_date = str_to_date('1996-01-01')
 live_start_date = str_to_date('2012-01-01')
 
-data_provider = ioc.container.resolve(GeneralPriceProvider)
+data_provider = container.resolve(GeneralPriceProvider)
 prices_tms = data_provider.get_price(QuandlTicker('AAPL', 'WIKI'), PriceField.Close, start_date)
 
 event_dates_list = [datetime(2000, 1, 1), datetime(2003, 1, 1), datetime(2006, 1, 1), datetime(2009, 1, 1)]
--- demo_scripts/charts/skewness_chart.py
+++ demo_scripts/charts/skewness_chart.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-import qf_common.config.ioc as ioc
+from demo_scripts.demo_configuration.demo_ioc import container
+
 from qf_lib.common.enums.price_field import PriceField
 from qf_lib.common.tickers.tickers import QuandlTicker, BloombergTicker
 from qf_lib.common.utils.dateutils.string_to_date import str_to_date
@@ -15,7 +16,7 @@ ticker = BloombergTicker('SPX Index')
 start_date = str_to_date('1996-01-01')
 end_date = datetime.now()
 
-data_provider = ioc.container.resolve(GeneralPriceProvider)
+data_provider = container.resolve(GeneralPriceProvider)
 prices_tms = data_provider.get_price(ticker, PriceField.Close, start_date, end_date)
 
 chart = create_skewness_chart(prices_tms)
