be930480480d807b15bf27e754e4fd5808c8b7a8
Marcin Borratynski <marcin.borratynski@cern.ch>
2019-03-29 18:23:52 +0100

    fixed all imports

--- demo_scripts/charts/qq_chart.py
+++ demo_scripts/charts/qq_chart.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-import qf_common.config.ioc as ioc
+from demo_scripts.demo_configuration.demo_ioc import container
 from qf_lib.common.enums.price_field import PriceField
 from qf_lib.common.tickers.tickers import QuandlTicker
 from qf_lib.common.utils.dateutils.string_to_date import str_to_date
@@ -12,7 +12,7 @@ from qf_lib.plotting.helpers.create_qq_chart import create_qq_chart
 start_date = str_to_date('2005-01-01')
 end_date = datetime.now()
 
-data_provider = ioc.container.resolve(GeneralPriceProvider)
+data_provider = container.resolve(GeneralPriceProvider)
 
 prices_tms = data_provider.get_price(QuandlTicker('AAPL', 'WIKI'), PriceField.Close, start_date, end_date)
 
