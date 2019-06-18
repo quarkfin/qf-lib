c46520e7931ac026daf7b51ec55bee3392b87580
Marcin Borratynski <marcin.borratynski@cern.ch>
2019-06-18 16:37:05 +0200

    MQP - vector weights and allocation tuning

--- qf_lib/common/utils/returns/max_drawdown.py
+++ qf_lib/common/utils/returns/max_drawdown.py
@@ -2,12 +2,13 @@ from typing import Union
 
 import pandas as pd
 
+from qf_lib.common.enums.frequency import Frequency
 from qf_lib.common.utils.returns.drawdown_tms import drawdown_tms
 from qf_lib.containers.dataframe.qf_dataframe import QFDataFrame
 from qf_lib.containers.series.qf_series import QFSeries
 
 
-def max_drawdown(input_data: Union[QFSeries, QFDataFrame]) -> Union[float, pd.Series]:
+def max_drawdown(input_data: Union[QFSeries, QFDataFrame], frequency: Frequency=None) -> Union[float, pd.Series]:
     """
     Finds maximal drawdown for the given timeseries of prices.
 
@@ -15,6 +16,8 @@ def max_drawdown(input_data: Union[QFSeries, QFDataFrame]) -> Union[float, pd.Se
     ----------
     input_data
         timeseries of prices/returns
+    frequency
+        optional parameter that improves teh performance of the function it is not need to infer the frequency
 
     Returns
     -------
@@ -22,4 +25,4 @@ def max_drawdown(input_data: Union[QFSeries, QFDataFrame]) -> Union[float, pd.Se
         maximal drawdown for the given timeseries of prices expressed as the percentage value (e.g. 0.5 corresponds
         to the 50% drawdown)
     """
-    return drawdown_tms(input_data).max()
+    return drawdown_tms(input_data, frequency=frequency).max()
