cdb0ebcadbc9301d722e0b1b06c96752ca9edc23
Karolina Cynk <karolina.kinga.cynk@cern.ch>
2019-08-14 16:08:51 +0200

    Added licensing information

--- qf_lib/backtesting/order/order.py
+++ qf_lib/backtesting/order/order.py
@@ -1,3 +1,17 @@
+#     Copyright 2016-present CERN – European Organization for Nuclear Research
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
 from qf_lib.backtesting.contract.contract import Contract
 from qf_lib.backtesting.order.execution_style import ExecutionStyle
 from qf_lib.backtesting.order.time_in_force import TimeInForce
