dae96fb8517742ed180670ef42aad6101ac93365
Karolina <karolinacynk@op.pl>
2022-04-07 17:00:44 +0200

    Minor fixes

--- demo_scripts/backtester/run_alpha_model_backtest_demo.py
+++ demo_scripts/backtester/run_alpha_model_backtest_demo.py
@@ -13,44 +13,52 @@
 #     limitations under the License.
 import matplotlib.pyplot as plt
 
-from demo_scripts.common.utils.dummy_ticker import DummyTicker
-from demo_scripts.demo_configuration.demo_data_provider import daily_data_provider
-from qf_lib.backtesting.position_sizer.initial_risk_position_sizer import InitialRiskPositionSizer
 from qf_lib.backtesting.strategies.signal_generators import OnBeforeMarketOpenSignalGeneration
 
-plt.ion()  # required for dynamic chart
+plt.ion()  # required for dynamic chart, good to keep this at the beginning of imports
 
 from demo_scripts.backtester.moving_average_alpha_model import MovingAverageAlphaModel
 from demo_scripts.demo_configuration.demo_ioc import container
 from qf_lib.backtesting.strategies.alpha_model_strategy import AlphaModelStrategy
+from qf_lib.backtesting.execution_handler.commission_models.ib_commission_model import IBCommissionModel
+from qf_lib.backtesting.position_sizer.initial_risk_position_sizer import InitialRiskPositionSizer
 from qf_lib.backtesting.trading_session.backtest_trading_session_builder import BacktestTradingSessionBuilder
 from qf_lib.common.enums.frequency import Frequency
+from qf_lib.common.tickers.tickers import QuandlTicker
 from qf_lib.common.utils.dateutils.string_to_date import str_to_date
 
 
 def main():
-    start_date = str_to_date("2010-01-01")
-    end_date = str_to_date("2015-03-01")
+    initial_risk = 0.03
+
+    start_date = str_to_date('2016-01-01')
+    end_date = str_to_date('2017-12-31')
 
     # ----- build trading session ----- #
-    session_builder: BacktestTradingSessionBuilder = container.resolve(BacktestTradingSessionBuilder)
+    session_builder = container.resolve(BacktestTradingSessionBuilder)  # type: BacktestTradingSessionBuilder
+    session_builder.set_backtest_name('Moving Average Alpha Model Backtest')
+    session_builder.set_position_sizer(InitialRiskPositionSizer, initial_risk=initial_risk)
+    session_builder.set_commission_model(IBCommissionModel)
     session_builder.set_frequency(Frequency.DAILY)
-    session_builder.set_data_provider(daily_data_provider)
-    session_builder.set_position_sizer(InitialRiskPositionSizer, initial_risk=0.05)
-
     ts = session_builder.build(start_date, end_date)
 
+    # ----- build models ----- #
     model = MovingAverageAlphaModel(fast_time_period=5, slow_time_period=20, risk_estimation_factor=1.25,
                                     data_provider=ts.data_handler)
-    model_tickers = [DummyTicker('AAA'), DummyTicker('BBB')]
+    model_tickers = [QuandlTicker('AAPL', 'WIKI'), QuandlTicker('AMZN', 'WIKI')]
     model_tickers_dict = {model: model_tickers}
 
     # ----- preload price data ----- #
     ts.use_data_preloading(model_tickers)
 
     # ----- start trading ----- #
-    OnBeforeMarketOpenSignalGeneration(AlphaModelStrategy(ts, model_tickers_dict))
+    OnBeforeMarketOpenSignalGeneration(AlphaModelStrategy(ts, model_tickers_dict, use_stop_losses=True))
     ts.start_trading()
+
+    # ----- use results ----- #
+    backtest_tms = ts.portfolio.portfolio_eod_series().to_log_returns()
+    print("mean daily log return: {}".format(backtest_tms.mean()))
+    print("std of daily log returns: {}".format(backtest_tms.std()))
 
 
 if __name__ == "__main__":
