ee2c85bf9a66cc1f716a652034f9ccf9302e19ed
tavisit <mateitavis@gmail.com>
2023-07-31 17:14:30 +0200

    Split the code base into multiple files

---
+++ qf_lib/documents_utils/document_exporting/element/helpers/style_enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class DataType(Enum):
+    ROW = 1
+    COLUMN = 2
+    INDEX = 3
+    CELL = 4
+    TABLE = 5
+
+
+class StylingType(Enum):
+    STYLE = 1
+    CLASS = 2
