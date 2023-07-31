e20cdd82ec51df03323d672d7ec61bf32e6a71e3
tavisit <mateitavis@gmail.com>
2023-07-31 17:13:51 +0200

    Added some comments

--- qf_lib/documents_utils/document_exporting/element/df_table.py
+++ qf_lib/documents_utils/document_exporting/element/df_table.py
@@ -11,10 +11,11 @@
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
-from enum import Enum
 from itertools import groupby
 from typing import Sequence, Optional, Union, Dict, Any, Tuple
 
+from documents_utils.document_exporting.element.helpers.style import Style, ColumnStyle, RowStyle, CellStyle
+from documents_utils.document_exporting.element.helpers.style_enums import DataType, StylingType
 from qf_lib.common.enums.grid_proportion import GridProportion
 from qf_lib.common.utils.logging.qf_parent_logger import qf_logger
 from qf_lib.common.utils.miscellaneous.to_list_conversion import convert_to_list
@@ -23,19 +24,6 @@ from qf_lib.containers.series.qf_series import QFSeries
 from qf_lib.documents_utils.document_exporting.document import Document
 from qf_lib.documents_utils.document_exporting.element import Element
 from qf_lib.documents_utils.document_exporting import templates
-
-
-class DataType(Enum):
-    ROW = 1
-    COLUMN = 2
-    INDEX = 3
-    CELL = 4
-    TABLE = 5
-
-
-class StylingType(Enum):
-    STYLE = 1
-    CLASS = 2
 
 
 class DFTable(Element):
@@ -201,24 +189,37 @@ class ModelController:
         self.table_styles = Style()
         self.index_styling = None
 
-    def add_styles_classes(self, element, data_to_update, styling_type: StylingType, modify_data: bool = True):
-        if styling_type == StylingType.STYLE:
-            if modify_data:
-                element.add_styles(data_to_update)
-            else:
-                element.remove_styles(data_to_update)
-        elif styling_type == StylingType.CLASS:
-            data_to_update, _ = convert_to_list(data_to_update, str)
-            if modify_data:
-                element.add_css_class(data_to_update)
-            else:
-                element.remove_css_class(data_to_update)
-
     def modify_data(self, location: Optional[Union[Any, Sequence[Any], Tuple[Any, Any]]] = None,
                     data_to_update: Union[str, Dict[str, str], Sequence[str]] = None,
                     data_type: DataType = DataType.TABLE,
                     styling_type: StylingType = StylingType.STYLE,
                     modify_data: bool = True):
+        """
+        Main method that modifies the css style and/or class of different elements in the ModelController
+        Parameters
+        ----------
+        location: Optional[Union[Any, Sequence[Any], Tuple[Any, Any]]]
+            Should be the following, regarding the type of data_type:
+            - column: Union[str, Sequence[str]]
+            - rows: Union[Any, Sequence[Any]]
+            - cells: Tuple[column, rows]
+            - table: None
+            - index: None
+            Default is None
+        data_to_update: Union[str, Dict[str, str], Sequence[str]]
+            The actual css information that will be inserted/deleted from the model.
+            Default is None
+        data_type: DataType
+            The type of data that needs to be modified.
+            Default is DataType.TABLE
+        styling_type: StylingType
+            The type of styling that needs to be modified.
+            Default is StylingType.STYLE
+        modify_data: bool
+            The type of data modification:
+            - if True, then it is adding to the current list of css
+            - if False, then it is removing from the current list of css
+        """
         def get_target_styles(location):
             if data_type == DataType.INDEX:
                 return self.index_styling or Style()
@@ -242,14 +243,13 @@ class ModelController:
                 return [self.table_styles]
 
         for target_style in get_target_styles(location):
-            self.add_styles_classes(target_style, data_to_update, styling_type, modify_data)
+            self._add_styles_classes(target_style, data_to_update, styling_type, modify_data)
 
     def iterrows(self):
         return zip(self.data.iterrows(), self.styles.iterrows())
 
     @property
     def styles(self):
-
         def get_cell(row_style, column_style, column_name):
             try:
                 row_indexer = row_style.label
@@ -298,90 +298,17 @@ class ModelController:
 
         return self._rows_styles
 
-
-class Style:
-    def __init__(self, style: Dict[str, str] = None, css_class: str = None):
-        self.style = style if style is not None else dict()
-        self.css_class = css_class.split() if css_class is not None else []
-        self.logger = qf_logger.getChild(self.__class__.__name__)
-
-    def add_css_class(self, css_classes: Sequence[str]):
-        self.css_class.extend(css_classes)
-
-    def remove_css_class(self, css_classes: Sequence[str]):
-        for class_name in css_classes:
-            try:
-                self.css_class.remove(class_name)
-            except ValueError:
-                self.logger.warning("The css class {} can not be removed, as it does not exist".format(class_name))
-
-    def add_styles(self, styles_dict: Dict[str, str]):
-        styles_dict = {key.replace(" ", ""): value.replace(" ", "") for key, value in styles_dict.items()}
-        self.style.update(styles_dict)
-
-    def remove_styles(self, styles: Union[Dict[str, str], Sequence[str]]):
-        properties = styles.keys() if type(styles) is dict else styles
-        for property_name in properties:
-            try:
-                del self.style[property_name]
-            except KeyError:
-                self.logger.warning("The css style for proptyety {} can not be removed, as it does not exist".
-                                    format(property_name))
-
-    def styles(self):
-        # The function merges into string all styles. This string may be further used as the css styles attributes
-        # value. No spaces between attributes and their values are allowed.
-        def merge_styles(styles_dict: Dict[str, str]) -> str:
-            return "".join(['%s:%s;' % (key, value) for key, value in styles_dict.items()])
-
-        styles = merge_styles(self.style)
-        styles = '""' if len(styles) == 0 else styles
-
-        return styles
-
-    def classes(self):
-        def merge_classes(css_classes_list: Sequence[str]) -> str:
-            return " ".join(css_classes_list)
-
-        css_classes = merge_classes(self.css_class)
-        return css_classes
+    def _add_styles_classes(self, element, data_to_update, styling_type: StylingType, modify_data: bool = True):
+        if styling_type == StylingType.STYLE:
+            if modify_data:
+                element.add_styles(data_to_update)
+            else:
+                element.remove_styles(data_to_update)
+        elif styling_type == StylingType.CLASS:
+            data_to_update, _ = convert_to_list(data_to_update, str)
+            if modify_data:
+                element.add_css_class(data_to_update)
+            else:
+                element.remove_css_class(data_to_update)
 
 
-class ColumnStyle(Style):
-    def __init__(self, column_name: str, style: Dict[str, str] = None, css_class: str = None):
-        super().__init__(style, css_class)
-        self.column_name = column_name
-
-
-class RowStyle(Style):
-    def __init__(self, label: Any, style: Dict[str, str] = None, css_class: str = None):
-        super().__init__(style, css_class)
-        self.label = label
-
-
-class CellStyle(Style):
-    def __init__(self, row_style, column_style, style: Dict[str, str] = None, css_class: str = None):
-        super().__init__(style, css_class)
-        self.row_style = row_style
-        self.column_style = column_style
-
-    def styles(self):
-        # The function merges into string all styles, corresponding to the row, column and cell.
-        styles = super().styles()
-
-        styles = self.row_style.styles() + self.column_style.styles() + styles
-        styles = styles.replace('""', '')
-        styles = '""' if len(styles) == 0 else styles
-
-        return styles
-
-    def classes(self):
-        # The function merges into string all styles, corresponding to the row, column and cell. This string may be
-        # further used as the css class attribute value
-        classes = super().classes()
-
-        css_classes = self.row_style.classes() + self.column_style.classes() + classes
-        css_classes = css_classes.replace('""', '')
-        css_classes = '""' if len(css_classes) == 0 else css_classes
-
-        return css_classes
