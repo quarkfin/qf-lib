89ef0df47948d8d067a857b394b182e68c8eeaba
Zeynep Gultug Aydemir <zeynep.gultug.aydemir@cern.ch>
2023-09-14 11:48:33 +0200

    stlye changes in waterfall chart

--- qf_lib/plotting/charts/waterfall_chart.py
+++ qf_lib/plotting/charts/waterfall_chart.py
@@ -35,8 +35,10 @@ class WaterfallChart(Chart):
         self.cumulative_sum = np.cumsum(self.data.values)
         for index, value in enumerate(self.data.items()):
             self._plot_waterfall(index, value)
+        self.axes.set_title(self.title, y=1.04)
 
         # Set x-axis label using 'category' column
+        self.axes.tick_params(axis='both', which='major', labelsize=10)
         self.axes.set_xticks(range(len(self.data.index) + 2))
         self.axes.set_xticklabels(['', *self.data.index, ''])
 
@@ -44,12 +46,15 @@ class WaterfallChart(Chart):
 
         # Bar color is determined based on whether there has been an increase, decrease,
         # or it represents a total column.
-        color = '#A6A6A6' if value[0] in self.total else '#4472C4' if value[1] > 0 else '#ED7D31'
+        color = '#A6A6A6' if any([x in value[0] for x in self.total]) else '#4472C4' if value[1] > 0 else '#ED7D31'
 
         if index == 0 or value[0] in self.total:
             self.axes.bar(index + 1, value[1], color=color)
+            self.axes.text(index + 1, value[1] + 0.02, value[1], ha='center', va='bottom', fontsize=10)
+
         else:
             self.axes.bar(index + 1, value[1], bottom=self.cumulative_sum[index - 1], color=color)
+            self.axes.text(index + 1, self.cumulative_sum[index] + 0.02, value[1], ha='center', va='bottom', fontsize=10)
 
     def apply_data_element_decorators(self, data_element_decorators: List["DataElementDecorator"]):
         pass
