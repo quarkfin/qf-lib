80d25f1a5dc354fbce8ce886c60902d0c0a71560
Eirik Eythorsson <eirik.eythorsson@cern.ch>
2024-02-23 16:17:52 +0100

    Fix css theme override

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,12 +64,9 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-    ],
-}
+html_css_files = [
+    'theme_overrides.css',
+]
 
 # -- Extension configuration -------------------------------------------------
 autosummary_generate = True
