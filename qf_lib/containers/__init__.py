cd0e55ac932c83b882e9c10090bd327cda0f4725
Robert DeRienzo <rlderienzo@gmail.com>
2024-09-14 08:39:56 -0400

    Add module documentation for containers
    I found working with the provided containers to be difficult, so I added a bit of extra documentation that will hopefully help other users

--- qf_lib/containers/__init__.py
+++ qf_lib/containers/__init__.py
@@ -11,3 +11,16 @@
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
+
+"""
+This module offers a comprehensive set of containers for financial data analysis, including series, dataframes, data arrays, and futures-related objects. 
+
+Each container type is designed with specialized classes to handle various aspects of financial data, such as price series, return calculations, and futures contract management. 
+These classes streamline the processing and analysis of complex financial datasets, providing a robust framework for quantitative finance research.
+
+Notes:
+  - When constructing a container object, the input data is typically expected to be in a wide format. For example:
+    - A `PricesSeries` object might be initialized with a DataFrame where each column represents the price history of a different asset.
+    - A `QFDataArray` can be created using multi-dimensional arrays to represent time, asset, and other axes of data.
+
+"""
