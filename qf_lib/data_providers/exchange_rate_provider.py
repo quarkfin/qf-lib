bd86a3318d7ab1caadba075983a7e8895f20d069
Karolina Cynk <myrmarachne@users.noreply.github.com>
2025-05-23 15:52:13 +0200

    Updated the signatures of Exchagne Rate Provider and Tickers Universe Provider (#219)

--- qf_lib/data_providers/exchange_rate_provider.py
+++ qf_lib/data_providers/exchange_rate_provider.py
@@ -15,7 +15,7 @@ class ExchangeRateProvider(object, metaclass=ABCMeta):
 
     @abstractmethod
     def get_last_available_exchange_rate(self, base_currency: str, quote_currency: str,
-                                         frequency: Frequency = Frequency.DAILY):
+                                         frequency: Frequency = Frequency.DAILY, **kwargs) -> float:
         """
         Get last available exchange rate from the base currency to the quote currency in the provided frequency.
 
