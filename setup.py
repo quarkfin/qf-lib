f038f95b018033ee5f1154ea6f7f17ebc66e1305
Karolina Cynk <karolinacynk@op.pl>
2026-06-03 15:55:02 +0200

    Updated minimum yfinance version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
                          "requests>=2.25.1,<=2.31.0", "requests_oauthlib", "urllib3"],
         "blpapi": ["blpapi>=3.21.0,<=3.24.4"],
         "quandl": ["quandl>=3.6.1,<=3.7.0"],
-        "yfinance": ["yfinance>=0.2.55"],
+        "yfinance": ["yfinance>=0.2.60"],
         "detailed_analysis": ["statsmodels>=0.13.0,<0.14.0", "scipy>=1.6.3,<1.12.0", "cvxopt>=1.2.7,<=1.3.2",
                               "arch>=5.4,<=7.0"],
         "alpaca": ["alpaca-py>=0.37.0"]
